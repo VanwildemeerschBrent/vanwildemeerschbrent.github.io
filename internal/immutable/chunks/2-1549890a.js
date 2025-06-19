b9ff4668614fe2d09277b3d695322e68fde209e6
VanwildemeerschBrent <vanwildemeersch.brent@gmail.com>
2025-06-19 20:56:34 +0000

    Update from https://github.com/VanwildemeerschBrent/svelte-portfolio/commit/a868040aa7701a639f249b8683c382ac13061d69

---
+++ internal/immutable/chunks/2-1549890a.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-0f6a37b9.js";export{t as component};
--- internal/immutable/chunks/2-f67feca6.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-97e79b48.js";export{t as component};
